e0e82b9a46e4cd2e1f33861ac2e169e54b58f80e
Maxwell Carlson <bart4282819@gmail.com>
2019-02-11 18:12:57 -0800

    Have a super simple matrix class with a super simple matrix mul working.

--- Regress/Linear/Matrix.h
+++ Regress/Linear/Matrix.h
@@ -1,15 +1,104 @@
+#include <vector>
 
 template<class T>
 class Matrix
 {
-	int rows;
-	int columns;
+	int nrows;
+	int ncolumns;
+	std::vector<T> vals;
+
 public:
-	Matrix(int rows, int columns) :
-		rows{rows},
-		columns{columns}
-	{}
+	Matrix(int nrows, int ncolumns);
+
+	int rows()		const { return nrows; }
+	int columns()	const { return ncolumns; }
+
+	T& operator()(int row, int col);
+	const T& operator()(int row, int col) const;
+
+
+	Matrix<T> operator*(const Matrix& m2);
 
 	friend Matrix operator+(const Matrix& m1, const Matrix& m2);
-	friend Matrix operator*(const Matrix& m1, const Matrix& m2);
+	friend Matrix operator-(const Matrix& m1, const Matrix& m2);
+	//friend Matrix operator*(const Matrix& m1, const Matrix& m2);
+
+	friend void matrixMul(Matrix& m, const Matrix& m1, const Matrix& m2);
 };
+
+template<class T>
+inline Matrix<T>::Matrix(int nrows, int ncolumns) :
+	nrows{		nrows			},
+	ncolumns{	ncolumns		},
+	vals(		nrows * ncolumns)
+{
+}
+
+template<class T>
+inline T & Matrix<T>::operator()(int row, int col) 
+{
+	return vals[row * ncolumns + col];
+}
+
+template<class T>
+inline const T & Matrix<T>::operator()(int row, int col) const
+{
+	return vals[row * ncolumns + col];
+}
+
+template<class T>
+inline Matrix<T> operator+(const Matrix<T> & m1, const Matrix<T> & m2)
+{
+	Matrix<T> m{ m1.rows, m1.columns };
+
+	return m;
+}
+
+template<class T>
+inline Matrix<T> operator-(const Matrix<T> & m1, const Matrix<T> & m2)
+{
+	Matrix<T> m{ m1.rows, m1.columns };
+
+	return m;
+}
+
+template<class T>
+inline void matrixMul(Matrix<T> & m, const Matrix<T> & m1, const Matrix<T> & m2)
+{
+	for (int i = 0; i < m1.rows; ++i)
+		for (int j = 0; j < m2.columns; ++j)
+		{
+			int sum = 0;
+			for (int k = 0; k < m1.columns; ++k)
+				sum += m1(i, k) + m2(k, j);
+			m(i, j) = sum;
+		}
+}
+
+template<class T>
+inline Matrix<T> Matrix<T>::operator*(const Matrix & m2)
+{
+	Matrix<T> m{ nrows, m2.ncolumns };
+	//matrixMul(m, *this, m2);
+	
+	for (int i = 0; i < nrows; ++i)
+		for (int j = 0; j < m2.ncolumns; ++j)
+		{
+			int sum = 0;
+			for (int k = 0; k < ncolumns; ++k)
+				sum += this->operator()(i, k) * m2(k, j);
+			m(i, j) = sum;
+		}
+	
+	return m;
+}
+
+/*
+template<class T>
+inline Matrix<T> operator*(const Matrix<T> & m1, const Matrix<T> & m2)
+{
+	Matrix<T> m{ m1.rows, m2.columns };
+	matrixMul(m, m1, m2);
+	return m;
+}
+*/
--- Regress/Main.cpp
+++ Regress/Main.cpp
@@ -1,7 +1,21 @@
-
+#include "Linear\Matrix.h"
 
 int main()
 {
+	Matrix<int> m1{ 2, 2 };
+	Matrix<int> m2{ 2, 2 };
+
+	m1(0, 0) = 1;
+	m1(0, 1) = 2;
+	m1(1, 0) = 3;
+	m1(1, 1) = 4;
+
+	m2(0, 0) = 4;
+	m2(0, 1) = 3;
+	m2(1, 0) = 2;
+	m2(1, 1) = 1;
+
+	auto m3 = m1 * m2;
 
 	return 0;
 }
