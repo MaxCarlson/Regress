e6f338e32859df47ddd381db0c6a145b18e29944
Maxwell Carlson <bart4282819@gmail.com>
2019-02-22 21:56:25 -0800

    Have the general framwork for the expression templates sketched out in code

--- Regress/Linear/Expr.h
+++ Regress/Linear/Expr.h
@@ -5,22 +5,21 @@
 // (instead of returning many matrices that are throw away)
 
 
-template<class E>
-class BaseExpr
-{
 
-};
 
 template<class E>
-struct MatrixTBase : public BaseExpr<E>
+struct MatrixTBase
 { };
 
 template<class Type>
-class MatrixT : public MatrixTBase<MatrixT<Type>>
+class MatrixT : public MatrixTBase<Type>
 {
+	using Storage = std::vector<Type>;
+
 	int nrows;
 	int ncols;
-	std::vector<Type> vals;
+	Storage vals;
+
 
 public:
 	using value_type	= Type;
@@ -60,6 +59,86 @@ public:
 	int size() const { return vals.size(); }
 	int rows() const { return nrows; }
 	int cols() const { return ncols; }
+
+
+	struct col_iterator 
+	{
+		using iterator_category = std::random_access_iterator_tag;
+
+		col_iterator& operator++()
+		{
+			++it
+			return *this;
+		}
+
+		Storage::iterator it;
+	};
 };
+
+struct MatrixOpBase
+{
+	enum Op
+	{
+		PLUS,
+		MINUS,
+		MUL
+	};
+};
+
+template<class Type>
+class MatrixMultOp : public MatrixOpBase
+{
+
+};
+
+template<class Iter>
+class MatrixExpr
+{
+
+};
+
+template<class LIt, class RIt, class Op>
+class MatBinExpr
+{
+	LIt lit;
+	RIt rit;
+	Op op;
+
+
+	const int lhsRows;
+	const int rhsRows;
+	const int lhsCols;
+	const int rhsCols;
+
+public:
+
+	MatBinExpr(LIt lit, RIt rit, int lhsRows, int rhsRows, int lhsCols, int rhsCols, Op op) :
+		lit{ lit },
+		rit{ rit },
+		lhsRows{ lhsRows },
+		rhsRows{ rhsRows },
+		lhsCols{ lhsCols },
+		rhsCols{ rhsCols },
+		op{ op }
+	{}
+};
+
+template<class Type>
+MatrixExpr<MatBinExpr<
+	typename MatrixT<Type>::col_iterator, 
+	typename MatrixT<Type>::col_iterator,
+	MatrixMultOp<Type>>>
+	operator*(const MatrixT<Type>& lhs, const MatrixT<Type>& rhs) noexcept
+{
+	using RetType = MatrixExpr<MatBinExpr<
+		typename MatrixT<Type>::col_iterator,
+		typename MatrixT<Type>::col_iterator,
+		MatrixMultOp<Type>>>;
+	return RetType{MatBinExpr{ 
+		
+	}
+	
+	}
+}
 
 
