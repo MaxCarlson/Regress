d2c45669c7a06f2c607fe157fe19cfe8d8f6d4fb
Maxwell Carlson <bart4282819@gmail.com>
2019-03-07 23:35:17 -0800

    Converted all MatrixExpr tests to test both column.row major order matrixes. All tests passing

--- test/Expressions.cpp
+++ test/Expressions.cpp
@@ -42,6 +42,12 @@ namespace test
 			{7, 8, 9},
 		};
 
+		cMM lhs1c = {
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
+		};
+
 		rMM rhs1 = {
 			{1, 2, 3, 4},
 			{5, 6, 7, 8}
@@ -52,13 +58,33 @@ namespace test
 			{5, 6, 7, 8}
 		};
 
+		template<class Mat>
+		void addImpl(const Mat& lhs, const Mat& rhs)
+		{
+			Mat res1 = rhs + rhs;
+			Mat res2 = (lhs + lhs) + lhs;
+
+			Mat res1v = { {2, 4, 6}, {8, 10, 12} };
+			Mat res2v = { {3, 6}, {9, 12}, {15, 18} };
+
+			Assert::IsTrue(res1 == res1v);
+			Assert::IsTrue(res2 == res2v);
+		}
+
 		TEST_METHOD(Add)
 		{
-			rMM res1 = rhs + rhs;
-			rMM res2 = (lhs + lhs) + lhs;
+			addImpl(lhs, rhs);
+			addImpl(lhsc, rhsc);
+		}
 
-			rMM res1v = { {2, 4, 6}, {8, 10, 12} };
-			rMM res2v = { {3, 6}, {9, 12}, {15, 18} };
+		template<class Mat>
+		void subImpl(const Mat& lhs, const Mat& rhs)
+		{
+			Mat res1 = rhs - rhs;
+			Mat res2 = (lhs - lhs) - lhs;
+
+			Mat res1v = { {0, 0, 0}, {0, 0, 0} };
+			Mat res2v = { {-1, -2}, {-3, -4}, {-5, -6} };
 
 			Assert::IsTrue(res1 == res1v);
 			Assert::IsTrue(res2 == res2v);
@@ -66,14 +92,8 @@ namespace test
 
 		TEST_METHOD(Sub)
 		{
-			rMM res1 = rhs - rhs;
-			rMM res2 = (lhs - lhs) - lhs;
-
-			rMM res1v = { {0, 0, 0}, {0, 0, 0} };
-			rMM res2v = { {-1, -2}, {-3, -4}, {-5, -6} };
-
-			Assert::IsTrue(res1 == res1v);
-			Assert::IsTrue(res2 == res2v);
+			subImpl(lhs, rhs);
+			subImpl(lhsc, rhsc);
 		}
 
 		template<class Mat>
@@ -96,17 +116,18 @@ namespace test
 			mulImpl(lhsc, rhsc, rhs1c);
 		}
 
-		TEST_METHOD(Constant)
+		template<class Mat>
+		void constantImpl(const Mat& lhs, const Mat& rhs)
 		{
-			rMM res1 = lhs + 2;
-			rMM res2 = rhs + 2 - 3;
-			rMM res3 = rhs * 3;
-			rMM res4 = 2 + rhs * 3;
+			Mat res1 = lhs + 2;
+			Mat res2 = rhs + 2 - 3;
+			Mat res3 = rhs * 3;
+			Mat res4 = 2 + rhs * 3;
 
-			rMM res1v = { {3, 4}, {5, 6}, {7, 8} };
-			rMM res2v = { {0, 1, 2}, {3, 4, 5} };
-			rMM res3v = { {3, 6, 9}, {12, 15, 18} };
-			rMM res4v = { {5, 8, 11}, {14, 17, 20} };
+			Mat res1v = { {3, 4}, {5, 6}, {7, 8} };
+			Mat res2v = { {0, 1, 2}, {3, 4, 5} };
+			Mat res3v = { {3, 6, 9}, {12, 15, 18} };
+			Mat res4v = { {5, 8, 11}, {14, 17, 20} };
 
 			Assert::IsTrue(res1 == res1v);
 			Assert::IsTrue(res2 == res2v);
@@ -114,35 +135,47 @@ namespace test
 			Assert::IsTrue(res4 == res4v);
 		}
 
-
-		TEST_METHOD(Transpose)
+		TEST_METHOD(Constant)
 		{
-			rMM res1 = ~lhs;
-			rMM res2 = ~lhs1;
+			constantImpl(lhs, rhs);
+			constantImpl(lhsc, rhsc);
+		}
 
-			rMM res1v = { {1, 3, 5}, {2, 4, 6} };
-			rMM res2v = { {1, 4, 7}, {2, 5, 8}, {3, 6, 9} };
+		template<class Mat>
+		void transposeImpl(const Mat& lhs, const Mat& lhs1)
+		{
+			Mat res1 = ~lhs;
+			Mat res2 = ~lhs1;
+
+			Mat res1v = { {1, 3, 5}, {2, 4, 6} };
+			Mat res2v = { {1, 4, 7}, {2, 5, 8}, {3, 6, 9} };
 
 			Assert::IsTrue(res1 == res1v);
 			Assert::IsTrue(res2 == res2v);
 		}
 
-		TEST_METHOD(Mixed)
+		TEST_METHOD(Transpose)
 		{
-			rMM res1 = ~lhs + rhs;
-			rMM res2 = ~rhs * ~lhs;
-			rMM res3 = (~rhs * ~lhs) * lhs1;
-			rMM res4 = (~rhs * ~lhs) - lhs1;
-			rMM res5 = ((~rhs * ~lhs) - lhs1) * 2;
-			rMM res6 = ((~rhs * ~lhs) - lhs1) + (lhs * rhs - lhs1);
+			transposeImpl(lhs, lhs1);
+			transposeImpl(lhsc, lhs1c);
+		}
 
-			rMM res1v = { {2, 5, 8}, {6, 9, 12} };
-			rMM res2v = { {9, 19, 29}, {12, 26, 40}, {15, 33, 51} };
-			rMM res3v = { {288, 345, 402}, {396, 474, 552}, {504, 603, 702} };
-			rMM res4v = { {8, 17, 26}, {8, 21, 34}, {8, 25, 42} };
-			rMM res5v = { {16, 34, 52}, {16, 42, 68}, {16, 50, 84} };
-			rMM res6v = { {16, 27, 38}, {23, 42, 61}, {30, 57, 84} };
+		template<class Mat>
+		void mixedImpl(const Mat& lhs, const Mat& lhs1, const Mat& rhs)
+		{
+			Mat res1 = ~lhs + rhs;
+			Mat res2 = ~rhs * ~lhs;
+			Mat res3 = (~rhs * ~lhs) * lhs1;
+			Mat res4 = (~rhs * ~lhs) - lhs1;
+			Mat res5 = ((~rhs * ~lhs) - lhs1) * 2;
+			Mat res6 = ((~rhs * ~lhs) - lhs1) + (lhs * rhs - lhs1);
 
+			Mat res1v = { {2, 5, 8}, {6, 9, 12} };
+			Mat res2v = { {9, 19, 29}, {12, 26, 40}, {15, 33, 51} };
+			Mat res3v = { {288, 345, 402}, {396, 474, 552}, {504, 603, 702} };
+			Mat res4v = { {8, 17, 26}, {8, 21, 34}, {8, 25, 42} };
+			Mat res5v = { {16, 34, 52}, {16, 42, 68}, {16, 50, 84} };
+			Mat res6v = { {16, 27, 38}, {23, 42, 61}, {30, 57, 84} };
 
 			Assert::IsTrue(res1 == res1v);
 			Assert::IsTrue(res2 == res2v);
@@ -150,6 +183,12 @@ namespace test
 			Assert::IsTrue(res4 == res4v);
 			Assert::IsTrue(res5 == res5v);
 			Assert::IsTrue(res6 == res6v);
+		}
+
+		TEST_METHOD(Mixed)
+		{
+			mixedImpl(lhs, lhs1, rhs);
+			mixedImpl(lhsc, lhs1c, rhsc);
 		}
 	};
 }
